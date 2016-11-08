e0b48060c8fc8d5653de416869932ae2f2906b58
Vincent Gagnon <vincentgagnon53@gmail.com>
2016-11-08 17:03:17 -0500

    Il manque encore quelques classes

--- src/Model/Vaisseau.java
+++ src/Model/Vaisseau.java
@@ -14,6 +14,59 @@ public class Vaisseau {
 
     public Vaisseau(ImageView iv){
         this.image = iv;
-        
+        this.vitesseX = 0;
+        this.vitesseY = 0;
+        this.X = 650;
+        this.Y = 50;
+        this.carburant = 1000;
+    }
+
+    public Bounds getBV() {
+        return BV;
+    }
+
+    public ImageView getImage() {
+        return image;
+    }
+
+    public double getVitesseY() {
+
+        return vitesseY;
+    }
+
+    public void setVitesseY(double vitesseY) {
+        this.vitesseY = vitesseY;
+    }
+
+    public double getVitesseX() {
+        return vitesseX;
+    }
+
+    public void setVitesseX(double vitesseX) {
+        this.vitesseX = vitesseX;
+    }
+
+    public double getY() {
+        return Y;
+    }
+
+    public void setY(double y) {
+        Y = y;
+    }
+
+    public double getX() {
+        return X;
+    }
+
+    public void setX(double x) {
+        X = x;
+    }
+
+    public double getCarburant() {
+        return carburant;
+    }
+
+    public void setCarburant(double carburant) {
+        this.carburant = carburant;
     }
 }
