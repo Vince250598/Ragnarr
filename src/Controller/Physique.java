4df97e58db6edcba38fe28ec1db7d149678b1d7b
Vincent Gagnon <vincentgagnon53@gmail.com>
2016-11-10 14:14:28 -0500

    Ajout des autres classes + le vaisseau tombe à cause de la gravité

--- src/Controller/Physique.java
+++ src/Controller/Physique.java
@@ -1,5 +1,70 @@
 package Controller;
 
-public class Physique {
+import Model.Planete;
+import Model.Vaisseau;
+import javafx.animation.Animation;
+import javafx.animation.KeyFrame;
+import javafx.animation.Timeline;
+import javafx.util.Duration;
 
+public class Physique {
+    private Vaisseau vaisseau;
+    private Planete planete;
+    private Timeline time;
+    private double temps = 0;
+    private double temps2 = 0;
+    private double gravite;
+
+
+
+    public Physique(Vaisseau v) {
+        this.vaisseau = v;
+        time = new Timeline(new KeyFrame(Duration.millis(10), a -> {
+            temps += 0.01;
+            temps2 += 0.01;
+        }));
+        planete = new Planete();
+        time.setCycleCount(Animation.INDEFINITE);
+        time.play();
+        setGravite(getPlanete().getGRAVITE());
+    }
+
+    public double calculVitesseY() {
+        getVaisseau().setVitesseY(getVaisseau().getVitesseY() + getPlanete().getGRAVITE() * temps2);
+        return getVaisseau().getVitesseY();
+    }
+
+    public double calculPosY() {
+        double depY = (calculVitesseY() * temps+0.5 * getPlanete().getGRAVITE() * Math.pow(temps, 2));
+        vaisseau.setY(vaisseau.getY() + depY);
+        return vaisseau.getY();
+    }
+
+    public Vaisseau getVaisseau() {
+        return vaisseau;
+    }
+
+    public void setGravite(double gravite) {
+        this.gravite = gravite;
+    }
+
+    public Planete getPlanete() {
+        return planete;
+    }
+
+    public Timeline getTime() {
+        return time;
+    }
+
+    public double getTemps() {
+        return temps;
+    }
+
+    public double getTemps2() {
+        return temps2;
+    }
+
+    public double getGravite() {
+        return gravite;
+    }
 }
--- src/View/Visuel.java
+++ src/View/Visuel.java
@@ -4,18 +4,18 @@ import javafx.geometry.Point2D;
 import javafx.scene.image.Image;
 import javafx.scene.image.ImageView;
 
-import java.io.File;
 import java.util.Vector;
 
 public class Visuel {
 
-    private ImageView rocket;
-    private Vector <Point2D> listePoints = new Vector<>();
 
-    public Visuel(){
-        File file = new File("/Ressources/rocket.png");
-        Image img = new Image(file.toURI().toString());
-        getRocket().setImage(img);
+    private Vector<Point2D> listePoints = new Vector<>();
+    Image img = new Image(getClass().getResource("/Ressources/rocket.png").toString());
+    private ImageView rocket = new ImageView(img);
+
+    public Visuel() {
+        getRocket().setScaleX(0.7);
+        getRocket().setScaleY(0.7);
     }
 
     public ImageView getRocket() {
